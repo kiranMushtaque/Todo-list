8eb3c2aec391a074118bb96adc03f93374348fd6
kiranMushtaque <160736950+kiranMushtaque@users.noreply.github.com>
2024-03-31 22:22:29 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,26 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+let todoList = [];
+let conditions = true;
+console.log(chalk.magenta.bgGreen.bold("\n \t Wellcome to Todo List\n"));
+while (conditions) {
+    let addTask = await inquirer.prompt([
+        { name: "task",
+            type: "input",
+            message: chalk.blue("Enter your New Task :")
+        },
+    ]);
+    todoList.push(addTask.task);
+    console.log(`${addTask.task}Task added in Todo-List succcessfully`);
+    let addMoreTask = await inquirer.prompt([
+        {
+            name: "addmore",
+            type: "confirm",
+            message: "Do you want to add more task ?",
+            default: "False,",
+        },
+    ]);
+    conditions = addMoreTask.addmore;
+}
+console.log("Your updated Todo-List:", todoList);
